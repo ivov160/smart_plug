27e11cb4e90bc2555fa99763872143f5eb62f8b6
ivov160 <ivov160@gmail.com>
2016-08-05 18:03:43 +0500

    added wifi generation

--- user/wifi_station.c
+++ user/wifi_station.c
@@ -10,27 +10,66 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
-#define MAX_AP_CONNECTION 10
+#include "flash.h"
 
-/*LOCAL struct station_config *sta_config = NULL;*/
+#define MAX_AP_CONNECTION 10
+#define PASSWORD_LENGTH 6
+#define MAX_SSID_LENGTH 32
+
 LOCAL struct softap_config* ap_config = NULL;
 
-void start_wifi()
+LOCAL char int_to_hex[] = "0123456789ABCDEF";
+
+void ICACHE_FLASH_ATTR generate_password(char* password, uint32_t size)
+{
+	uint8_t current_size = 0;
+	while(current_size < size)
+	{
+		unsigned long number = os_random();
+		uint8_t value = number % 16;
+		
+		/*if(strchar(password, value) == NULL)*/
+		/*{*/
+			password[current_size] = int_to_hex[value];
+			++current_size;
+		/*}*/
+	}
+}
+
+void ICACHE_FLASH_ATTR start_wifi()
 {
 	if(ap_config == NULL)
 	{
-		ap_config = (struct softap_config *)zalloc(sizeof(struct softap_config));
-		/*if(!wifi_softap_get_config(ap_config))*/
+		/*struct device_info device_info;*/
+		/*memset(&device_info, 0, sizeof(struct device_info));*/
+
+		/*if(!read_current_device(&device_info))*/
 		/*{*/
-			/*os_printf("failed getting softap_config\n");*/
+			/*os_printf("wifi: failed get current device info\n");*/
 			/*return;*/
 		/*}*/
 
-		char ssid[10] = { 0 };
-		char password[10] = { 0 };
+		/*const char* device_type_str = device_info_get_type(&device_info);*/
+		char device_type_str[] = "PLUG";
+		char ssid[MAX_SSID_LENGTH] = { 0 };
+		char password[PASSWORD_LENGTH + 1] = { 0 };
 
-		sprintf(ssid, "ESP_WIFI");
-		sprintf(password, "BmW24zQ5");
+		generate_password(password, PASSWORD_LENGTH);
+
+		int render_size = sprintf(ssid, "LOOKin_%s_%s", device_type_str, password);
+		/*int render_size = sprintf(ssid, "ESP_WIFI");*/
+		if(render_size < 0)
+		{
+			os_printf("wifi: failed generate ssid\n");
+			return;
+		}
+
+		ap_config = (struct softap_config *)zalloc(sizeof(struct softap_config));
+		if(!wifi_softap_get_config(ap_config))
+		{
+			os_printf("failed getting softap_config\n");
+			return;
+		}
 
 		memcpy(ap_config->ssid, ssid, strlen(ssid));
 		ap_config->ssid_len = strlen(ssid);
@@ -43,6 +82,10 @@ void start_wifi()
 
 		ap_config->max_connection = MAX_AP_CONNECTION;
 
+		os_printf("wifi: ssid: %s, render_size: %d, password: %s\n", ap_config->ssid, render_size, ap_config->password);
+
+		/*if(!wifi_softap_set_config(ap_config) || !wifi_softap_set_config_current(ap_config))*/
+		/*if(!wifi_softap_set_config_current(ap_config))*/
 		if(!wifi_softap_set_config(ap_config))
 		{
 			os_printf("failed set softap_config\n");
